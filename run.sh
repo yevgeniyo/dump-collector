8990bd6d2f80009e4b3a33cb117ddeb6ad925e08
Moshe Ben Shoham <mosheb@next-insurance.com>
2020-12-23 10:55:36 +0200

    changing dir max size to 10GB

--- run.sh
+++ run.sh
@@ -6,8 +6,8 @@ while true; do
 
     echo "Checking /dumps size on the host"
     dumps_size=`du -s /dumps | awk '{print $1}'`
-    if [ "$dumps_size" -ge 50000000 ]; then
-        echo "Dumps size more than 50G, cleaning /dumps"
+    if [ "$dumps_size" -ge 10000000 ]; then
+        echo "Dumps size more than 10G, cleaning /dumps"
         rm -rf /dumps/*
     else echo "/dumps size is $dumps_size bytes and less than 50000000 (50G), nothing to do"
     fi
