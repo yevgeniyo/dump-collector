4ca92c63df005e367995f25f2d7f96a436856c78
Moshe Ben Shoham <mosheb@next-insurance.com>
2020-12-24 15:54:11 +0200

    testing error message

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 while true; do
-    rclone move /dumps remote:$S3bucket
+
+    now=$(date +%Y%m%d%H%M%S)
+    rclone move /dumps remote:$S3bucket/$(date +%Y_%m_%d_%H_%M_%S)
     RESULT=$?
     if [ $RESULT -ne 0 ]; then
         echo "Failed to send dump files to S3 bucket ${S3bucket}"
