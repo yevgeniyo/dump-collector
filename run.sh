808808ff217ea55a986b19a7123c85d5e0b19cd6
Moshe Ben Shoham <mosheb@next-insurance.com>
2020-12-24 15:00:15 +0200

    using move instead of copy

--- run.sh
+++ run.sh
@@ -5,13 +5,13 @@ while true; do
     #TODO delete old files, add timestamps
     rclone move /dumps remote:$S3bucket
 
-    echo "Checking /dumps size on the host"
-    dumps_size=`du -s /dumps | awk '{print $1}'`
-    if [ "$dumps_size" -ge 10000000 ]; then
-        echo "Dumps size more than 10G, cleaning /dumps"
-        rm -rf /dumps/*
-    else echo "/dumps size is $dumps_size bytes and less than 10000000 (10G), nothing to do"
-    fi
+#    echo "Checking /dumps size on the host"
+#    dumps_size=`du -s /dumps | awk '{print $1}'`
+#    if [ "$dumps_size" -ge 10000000 ]; then
+#        echo "Dumps size more than 10G, cleaning /dumps"
+#        rm -rf /dumps/*
+#    else echo "/dumps size is $dumps_size bytes and less than 10000000 (10G), nothing to do"
+#    fi
 
     sleep 60s
 done
