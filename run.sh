07c5b4b98c859a3127cc51150c36c17e0cdacbb7
Moshe Sayag <moshe.s@next-insurance.com>
2021-11-25 15:01:33 +0200

    Move directories as is. Move files to a directory named with the file timestamp

--- run.sh
+++ run.sh
@@ -1,14 +1,23 @@
 #!/usr/bin/env bash
 
 while true; do
-
-    now=$(date +%Y%m%d%H%M%S)
-    rclone move /dumps remote:$S3bucket/$(date +%Y_%m_%d_%H_%M_%S)
-    RESULT=$?
-    if [ $RESULT -ne 0 ]; then
-        echo "Failed to send dump files to S3 bucket ${S3bucket}"
-    fi
-
+    for FILE in /dumps/*; do
+        if [ -d $FILE ]; then
+            # directory: move as is
+            rclone move /dumps/$FILE remote:$S3bucket
+            RESULT=$?
+            if [ $RESULT -ne 0 ]; then
+                echo "Failed to send dump dir /dumps/$FILE to S3 bucket $S3bucket"
+            fi
+        else
+            # file: move to a directory named with the file timestamp
+            TIMESTAMP=$(stat -f "%Sm" -t "%Y-%m-%d-%H-%M-%S" $FILE)
+            rclone move /dumps/$FILE remote:$S3bucket/$TIMESTAMP
+            RESULT=$?
+            if [ $RESULT -ne 0 ]; then
+                echo "Failed to send dump file /dumps/$FILE to S3 bucket $S3bucket"
+            fi
+        fi
+    done
     sleep 60s
 done
-
