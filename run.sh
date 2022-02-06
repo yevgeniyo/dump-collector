a86702d6e8e9417d9a91b1299d3d06781ffa7080
Moshe Sayag <moshe.s@next-insurance.com>
2022-02-06 10:45:57 +0200

    Fixed acl and remove "dumps" from the target path

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
+cd /dumps || exit
 while true; do
-    for FILE in /dumps/*; do
+    for FILE in *; do
         if [ -d $FILE ]; then
             # directory: move as is
             DIR=$(basename $FILE)
-            aws s3 mv $FILE s3://$S3bucket/$DIR --recursive
+            aws s3 mv $FILE s3://$S3bucket/$DIR --recursive --acl bucket-owner-full-control
             RESULT=$?
             if [ $RESULT -ne 0 ]; then
                 echo "Failed to send dump dir $FILE to S3 bucket $S3bucket"
@@ -13,7 +14,7 @@ while true; do
         elif [ -f $FILE ]; then
             # file: move to a directory named with the file timestamp
             TIMESTAMP=$(date -r $FILE +"%Y_%m_%d_%H_%M_%S")
-            aws s3 mv $FILE s3://$S3bucket/$TIMESTAMP/$FILE
+            aws s3 mv $FILE s3://$S3bucket/$TIMESTAMP/$FILE --acl bucket-owner-full-control
             RESULT=$?
             if [ $RESULT -ne 0 ]; then
                 echo "Failed to send dump file $FILE to S3 bucket $S3bucket"
